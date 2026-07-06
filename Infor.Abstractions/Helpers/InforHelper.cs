efc3ad266384a90e5eed1a3072ad0bf50c71793f
dgutierrez <dgutierrez@DGUTIERREZ1>
2026-07-06 18:01:01 -0500

    Implementación de helpers para conversión de datos fecha y decimal en Infor Cloud

---
+++ Infor.Abstractions/Helpers/InforHelper.cs
@@ -0,0 +1,35 @@
+﻿using System.Globalization;
+
+namespace Infor.Abstractions.Helpers
+{
+    public class InforHelper
+    {
+        public static decimal? ToDecimal(string? value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                return null;
+
+            if (decimal.TryParse(
+                value,
+                NumberStyles.Any,
+                CultureInfo.InvariantCulture,
+                out var result))
+            {
+                return result;
+            }
+
+            return null;
+        }
+        private const string formatDate = "yyyyMMdd HH:mm:ss.fff";
+        public static DateTime? ToDateTime(string? value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                return null;
+
+            return DateTime.ParseExact(
+                value,
+                formatDate,
+                CultureInfo.InvariantCulture);
+        }
+    }
+}
