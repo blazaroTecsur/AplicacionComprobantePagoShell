b084b0ebe24795819d4a3160330b7e396ba20765
Bryam Lazaro Yepez <blazaro@tecsur.local>
2026-08-20 15:26:14 -0500

    fix: update versión nugget seguridad y maestros

--- ComprobantePago.Infrastructure/Services/Maestros/ApiCuentaContableService.cs
+++ ComprobantePago.Infrastructure/Services/Maestros/ApiCuentaContableService.cs
@@ -1,14 +1,16 @@
 using ComprobantePago.Application.DTOs.Comprobante.Common;
 using ComprobantePago.Application.Interfaces.Services.Maestros;
 using Maestro.Abstractions.Interfaces;
+using Seguridad.Abstractions.Interfaces;
 
 namespace ComprobantePago.Infrastructure.Services.Maestros
 {
-    public class ApiCuentaContableService(IMaestrosCuentaContableService maestros) : ICuentaContableService
+    public class ApiCuentaContableService(IMaestrosCuentaContableService maestros,
+        IUsuarioContexto usuario) : ICuentaContableService
     {
         public async Task<IEnumerable<ComboDto>> ObtenerCuentasContablesAsync(string filtro = "")
         {
-            var result = await maestros.GetAllAsync(filtro, 1, 100);
+            var result = await maestros.GetAllAsync(usuario.Esquema, filtro, 1, 100);
             return result.Items
                 .Where(c => c.Codigo.Length >= 7)
                 .Select(c => new ComboDto
--- ComprobantePago.Infrastructure/Services/Maestros/ApiProveedorService.cs
+++ ComprobantePago.Infrastructure/Services/Maestros/ApiProveedorService.cs
@@ -1,14 +1,16 @@
 using ComprobantePago.Application.DTOs.Comprobante.Common;
 using ComprobantePago.Application.Interfaces.Services.Maestros;
 using Maestro.Abstractions.Interfaces;
+using Seguridad.Abstractions.Interfaces;
 
 namespace ComprobantePago.Infrastructure.Services.Maestros
 {
-    public class ApiProveedorService(IMaestrosProveedorService maestros) : IProveedorService
+    public class ApiProveedorService(IMaestrosProveedorService maestros,
+        IUsuarioContexto usuario) : IProveedorService
     {
         public async Task<IEnumerable<ComboDto>> ObtenerProveedoresAsync(string filtro = "")
         {
-            var result = await maestros.GetAllAsync(filtro, 1, 100);
+            var result = await maestros.GetAllAsync(usuario.Esquema, filtro, 1, 100);
             return result.Items
                 .Where(p => p.Estado.ToUpper() != "INACTIVO")
                 .Select(p => new ComboDto
